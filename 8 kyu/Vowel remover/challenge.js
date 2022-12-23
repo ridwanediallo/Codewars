48c8212bb736f42fb6a2a33af4f115ab48d01bdb
ridwan <ridwanediallo99@gmail.com>
2022-12-23 20:01:25 +0000

    Add improved solution for vowel remove challenge

--- 8 kyu/Vowel remover/challenge.js
+++ 8 kyu/Vowel remover/challenge.js
@@ -1,5 +1,7 @@
 function shortcut(string) {
-  const vowelsFree = string.replace(/[aeiou]/gi, '');
-  return vowelsFree;
+//   const vowelsFree = string.replace(/[aeiou]/gi, '');
+//   return vowelsFree;
+
+    return string.split('').filter((letter => !'aeiou'.includes(letter))).join('');
 }
 console.log(shortcut('codewars'));
