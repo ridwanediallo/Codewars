04ebe76f230ff22d120cc828eb716f8d982a4cd3
ridwan <ridwanediallo99@gmail.com>
2022-12-27 16:14:49 +0000

    Add solution for Cat years, Dog years challenge

--- 8 kyu/Cat years, Dog years/challenge.rb
+++ 8 kyu/Cat years, Dog years/challenge.rb
@@ -1,4 +1,23 @@
 def human_years_cat_years_dog_years(human_years)
   # Your code here!
-  return [0,0,0]
+  cat_years = 0
+  dog_years = 0
+  ages = []
+
+
+   if human_years === 1
+     cat_years = 15
+     dog_years = 15
+    ages.push(human_years, cat_years, dog_years)
+   elsif human_years === 2
+     cat_years = 15 + 9
+     dog_years = 15 + 9
+     ages.push(human_years, cat_years, dog_years)
+   elsif human_years > 2
+    cat_years = 24 + (human_years - 2) * 4
+    dog_years = 24 + (human_years - 2) * 5
+    ages.push(human_years, cat_years, dog_years)
+   end
+
+  return ages
 end
