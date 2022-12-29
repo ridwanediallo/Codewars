37855dd2bcc250ad620ce2be4e93539466102114
ridwan <ridwanediallo99@gmail.com>
2022-12-29 11:30:00 +0000

    Add solution for Quarter of the year challenge

--- 8 kyu/Quarter of the year/challenge.rb
+++ 8 kyu/Quarter of the year/challenge.rb
@@ -1,3 +1,18 @@
 def quarter_of(month)
   # code here
+  def quarter_of(month)
+  # code here
+  quarter = 1
+
+  if month <= 3
+    quarter
+  elsif month > 3 && month <= 6
+    quarter = 2
+  elsif month > 6 && month <= 9
+    quarter = 3
+  else
+    quarter = 4
+  end
+  quarter
+end
 end
