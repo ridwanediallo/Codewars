21c088aee75e8192e129ed3a89b0ae2550953c11
ridwan <ridwanediallo99@gmail.com>
2023-01-05 12:00:49 +0000

    Add Filter out the geese challenge solution

--- 8 kyu/Filter out the geese/challenge.rb
+++ 8 kyu/Filter out the geese/challenge.rb
@@ -1,3 +1,7 @@
 def goose_filter (birds)
   geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]
+
+  puts birds - geese
 end
+
+goose_filter(["Mallard", "Hook Bill", "Crested", "Blue Swedish", "Roman Tufted"])
